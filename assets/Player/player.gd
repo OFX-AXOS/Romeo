6e3cd923c1a47cab26f4e909cba8778552a92351
Arjun Khanal <251812551+OFX-AXOS@users.noreply.github.com>
2026-01-24 17:48:49 +0545

    Added Files

---
+++ assets/Player/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+
+const SPEED = 110.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+
+	# Get the input direction (input_axis) and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_axis = Input.get_axis("left", "right")
+	if input_axis:
+		velocity.x = input_axis * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	update_animations(input_axis)
+
+
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = input_axis < 0
+		animated_sprite_2d.play("run")
+	elif  Input.is_action_pressed("crouch"):
+		animated_sprite_2d.play("crouch")
+		return
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
